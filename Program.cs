27967074c35fad8a14f07f52ecf68fdc6d8ebfc1
ddesjardins-splunk <ddesjardins@splunk.com>
2020-04-15 15:51:15 -0600

    initial commit of .net Sfx APM Training example.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SfxCurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
